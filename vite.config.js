9ab7efc1776de716149f821802c436a7c79b9ac9
Sajad Danish <Sajaddanish256@gmail.com>
2025-10-28 01:20:07 +0430

    comment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // same as '0.0.0.0', makes server accessible on LAN
+    port: 5173, // you can change this if needed
+    open: true, // don't open on PC if you want to just view on mobile
+  },
+});
